5fea2546d1c2a784fe6d03dd98c17132570bfcf6
Kris Krej <kris@krej.net>
2018-04-17 11:42:48 +0200

    taichi

---
+++ Assets/Code/Witcher/WitcherLogic.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WitcherLogic : MonoBehaviour {
+	public float GetKnobPositionInNextFrame(float currentPosition01, float mousePosition01) {
+        return mousePosition01;
+    }
+}
---
+++ Assets/Code/Witcher/WitcherVisualisation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WitcherVisualisation : MonoBehaviour {
+    public RectTransform knob;
+    public WitcherLogic logic;
+		
+	void FixedUpdate () {
+        var newPosition = logic.GetKnobPositionInNextFrame(GetKnobPosition(), GetMousePosition());
+        SetKnobPosition(newPosition);
+	}
+
+    private float GetKnobPosition() {
+        return knob.anchoredPosition.x;
+    }
+
+    private float GetMousePosition() {
+        return Input.mousePosition.x / Screen.width;
+    }
+
+    private void SetKnobPosition(float newPosition) {
+        knob.anchoredPosition = new Vector2(newPosition*Screen.width - Screen.width/2, knob.anchoredPosition.y);
+    }
+}
